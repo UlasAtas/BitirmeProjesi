46fb4b2c1cbc742a1fba634edc0727d9afc9c786
ursusarctosturcia <gencbora08@gmail.com>
2024-03-22 22:01:59 +0300

    Level1 animator hatası çözüdü

---
+++ Bitirme Projesi/Assets/Scripts/level1/RobotHareket.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotHareket : MonoBehaviour
+{
+    public float robot_hizi = 3.0f;
+    //private Animator animator;
+    public bool hareketEt;
+    public AudioSource hareketSes;
+
+    private void Start()
+    {
+        hareketEt = true;
+        //animator = GetComponent<Animator>();
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //if (hareketEt)
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(new Vector2(-1.0f, 0f) * robot_hizi * Time.deltaTime);
+                //animator.SetBool("moving", true);
+                hareketSes.Play();
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(new Vector2(1.0f, 0f) * robot_hizi * Time.deltaTime);
+                //animator.SetBool("moving", true);
+                hareketSes.Play();
+            }
+            else
+            {
+                //animator.SetBool("moving", false);
+                hareketSes.Pause();
+            }
+
+        }
+
+    }
+}
